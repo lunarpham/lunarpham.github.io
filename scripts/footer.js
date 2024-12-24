a5f3a2ae157b7454e4e28515f54641de9108e1c8
Trưởng Phạm <truong.pham210214@vnuk.edu.vn>
2024-12-25 02:11:53 +0700

    fix minor error

--- scripts/footer.js
+++ scripts/footer.js
@@ -2,5 +2,5 @@ export function renderFooter() {
     const footer = document.getElementById('footer');
     footer.innerHTML = `
         <div class="text-center">(c) 2024 - Lunar Pham</div>
-        <div class="text-center hover:underline"><a href="/">Back to top</a></div>
+        <div class="text-center hover:underline"><a href="#">Back to top</a></div>
     `}
